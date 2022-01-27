60800f5f1713fc8052e377eacab1ff46f33b06e3
Elxan Quliyev <qelxan177@gmail.com>
2022-01-27 02:42:27 -0800

    Add project files.

---
+++ Web/ViewModels/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+namespace Web.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Product>? SliderList { get; set; }
+        public List<Product>? ProductsList { get; set; }
+        public List<Product>? NewProducts { get; set; }
+        public List<Product> MonthProducts { get; set; }
+
+
+    }
+}
